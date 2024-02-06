098105ac2dcde5815d4d11f29caf4e51fe1c0222
Alex Jolly <alexjolly28@gmail.com>
2024-02-06 16:13:35 +0530

    main.rs added

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod hnsw;
+
+use hnsw::database::Database;
+
+// use std::cell::RefCell;
+
+
+// thread_local! {
+//     static DB:RefCell<Database> = RefCell::new(Database::new())
+
+// }
+
+fn main(){
+
+    let mut db: Database = Database::new();
+    let created = db.create_collection("name".to_string(), 3);
+    println!("{:?}", created);
+    let keys: Vec<Vec<f32>> = vec![vec![10.0,12.0,4.5],vec![10.0,11.0,10.5],vec![10.0,20.5,15.0]];
+    let values: Vec<String> = vec!["red".to_string(),"green".to_string(), "blue".to_string()];
+    db.insert_into_collection("name", keys, values);
+
+    let query_vec: Vec<f32>=vec![10.0,11.5,8.5];
+    let reslut = db.query("name",query_vec,100);
+    println!("results{:?}",reslut);
+
+
+}

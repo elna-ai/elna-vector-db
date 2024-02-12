7e6cce6fd69fd21c131ba84a9055db623ab09529
Alex Jolly <alexjolly28@gmail.com>
2024-02-12 22:43:35 +0530

    lib.rs file addd

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+pub mod hnsw;
+
+use hnsw::database::{Database,Error};
+use ic_cdk::{export_candid, query, update};
+
+
+use std::cell::RefCell;
+
+thread_local! {
+    static DB: RefCell<Database> = RefCell::new(Database::new())
+}
+
+
+#[update]
+fn create_collection(name: String, dimension: usize) -> Result<(), Error> {
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+        db.create_collection(name, dimension)
+    })
+}
+
+
+#[update]
+fn insert(name: String, keys: Vec<Vec<f32>>, values: Vec<String>) -> Result<(), Error> {
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+        db.insert_into_collection(&name,keys, values)
+    })
+}
+
+#[query]
+fn query(name:String,q: Vec<f32>, limit: i32) -> Vec<String> {
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+        let result = db.query(&name,q,limit);
+        match result {
+            Ok(data) => {
+                // Extract the Vec<(f32, String)> from the Ok variant
+                let (_, strings): (Vec<_>, Vec<_>) = data.into_iter().unzip();
+                // println!("Floats: {:?}", floats);
+                 strings
+            }
+            Err(error) => {
+                // Handle the error
+                println!("Error: {}", error);
+                vec![error]
+            }
+        }
+        
+    })
+
+}
+
+export_candid!();

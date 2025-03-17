b1fcdf5708aa49b63453d999c50dcd29f726bb97
Alex Jolly <alexjolly28@gmail.com>
2025-03-17 20:06:10 +0530

    calculate-cycles added

--- src/elna_db_backend/src/lib.rs
+++ src/elna_db_backend/src/lib.rs
@@ -1,24 +1,80 @@
 extern crate elna_auth_macros;
 
 mod database;
+use std::cell::RefCell;
+
+use candid::CandidType;
 use candid::Principal;
 use database::db::DB;
 use database::error::Error;
 use database::memory::get_upgrades_memory;
 use database::users::{ADMINS, OWNER};
 use elna_auth_macros::check_authorization;
+use ic_cdk::api::canister_balance;
 use ic_cdk::{post_upgrade, pre_upgrade, query, update};
 use ic_cdk_macros::export_candid;
 use ic_stable_structures::writer::Writer;
 use ic_stable_structures::Memory as _;
+use serde::Deserialize;
+
+thread_local! {
+    static LOGS: RefCell<Vec<LogEntry>> = RefCell::default();
+
+}
+
+#[derive(Deserialize, CandidType, Debug, Clone)]
+pub struct LogEntry {
+    timestamp: u64,
+    caller: Principal,
+    function_name: String,
+    total_cycles: u64,
+}
+
+//  implement Default for LogEntry
+impl Default for LogEntry {
+    fn default() -> Self {
+        LogEntry {
+            timestamp: 0,
+            caller: Principal::anonymous(), // Use a default Principal (e.g., anonymous)
+            function_name: String::new(),
+            total_cycles: 0,
+        }
+    }
+}
 
 #[update]
 #[check_authorization]
 fn create_collection(name: String, dimension: usize) -> Result<(), Error> {
-    DB.with(|db| {
+    let initial_cycles = canister_balance();
+    ic_cdk::println!("fn:create_collection: initial_cycles:{}", initial_cycles);
+
+    let result = DB.with(|db| {
         let mut db = db.borrow_mut();
         db.create_collection(&name, dimension)
-    })
+    });
+
+    // Get the final cycle count
+
+    let final_cycles = canister_balance();
+    ic_cdk::println!("fn:create_collection: final_cycles:{}", final_cycles);
+    // Calculate the cycles used
+    let cycles_used = initial_cycles - final_cycles;
+    ic_cdk::println!("fn:create_collection: cycles_used:{}", cycles_used);
+
+    // Create a new log entry
+    let log_entry = LogEntry {
+        timestamp: ic_cdk::api::time(), // Use the current timestamp
+        caller: ic_cdk::caller(),
+        function_name: "create_collection".to_string(),
+        total_cycles: cycles_used,
+    };
+
+    // Append the log entry to the LOGS
+    LOGS.with(|logs| {
+        logs.borrow_mut().push(log_entry);
+    });
+
+    result
 }
 
 #[update]
@@ -30,12 +86,38 @@ fn create_index(
     embeddings: Vec<Vec<f32>>,
     file_name: String,
 ) -> Result<(), Error> {
-    DB.with(|db| {
+    let initial_cycles = canister_balance();
+    ic_cdk::println!("fn:create_index: initial_cycles:{}", initial_cycles);
+
+    let result = DB.with(|db| {
         let mut db = db.borrow_mut();
-        db.create_collection(&name, dimension);
-        db.insert_into_collection(&name, embeddings, docs, file_name);
+        let _ = db.create_collection(&name, dimension);
+        let _ = db.insert_into_collection(&name, embeddings, docs, file_name);
         db.build_index(&name)
-    })
+    });
+
+    // Get the final cycle count
+
+    let final_cycles = canister_balance();
+    ic_cdk::println!("fn:create_index: final_cycles:{}", final_cycles);
+    // Calculate the cycles used
+    let cycles_used = initial_cycles - final_cycles;
+    ic_cdk::println!("fn:create_index: cycles_used:{}", cycles_used);
+
+    // Create a new log entry
+    let log_entry = LogEntry {
+        timestamp: ic_cdk::api::time(), // Use the current timestamp
+        caller: ic_cdk::caller(),
+        function_name: "create_index".to_string(),
+        total_cycles: cycles_used,
+    };
+
+    // Append the log entry to the LOGS
+    LOGS.with(|logs| {
+        logs.borrow_mut().push(log_entry);
+    });
+
+    result
 }
 
 #[update]
@@ -46,19 +128,69 @@ fn insert(
     values: Vec<String>,
     file_name: String,
 ) -> Result<(), Error> {
-    DB.with(|db| {
+    let initial_cycles = canister_balance();
+    ic_cdk::println!("fn:insert: initial_cycles:{}", initial_cycles);
+
+    let _ = DB.with(|db| {
         let mut db = db.borrow_mut();
         db.insert_into_collection(&name, keys, values, file_name)
-    })
+    });
+
+    // Get the final cycle count
+
+    let final_cycles = canister_balance();
+    ic_cdk::println!("fn:insert: final_cycles:{}", final_cycles);
+    // Calculate the cycles used
+    let cycles_used = initial_cycles - final_cycles;
+    ic_cdk::println!("fn:insert: cycles_used:{}", cycles_used);
+
+    // Create a new log entry
+    let log_entry = LogEntry {
+        timestamp: ic_cdk::api::time(), // Use the current timestamp
+        caller: ic_cdk::caller(),
+        function_name: "insert".to_string(),
+        total_cycles: cycles_used,
+    };
+
+    // Append the log entry to the LOGS
+    LOGS.with(|logs| {
+        logs.borrow_mut().push(log_entry);
+    });
+    Ok(())
 }
 
 #[update]
 #[check_authorization]
+
 fn build_index(name: String) -> Result<(), Error> {
-    DB.with(|db| {
+    let initial_cycles = canister_balance();
+    ic_cdk::println!("fn:build_index: initial_cycles:{}", initial_cycles);
+    let _ = DB.with(|db| {
         let mut db = db.borrow_mut();
         db.build_index(&name)
-    })
+    });
+
+    // Get the final cycle count
+
+    let final_cycles = canister_balance();
+    ic_cdk::println!("fn:build_index: final_cycles:{}", final_cycles);
+    // Calculate the cycles used
+    let cycles_used = initial_cycles - final_cycles;
+    ic_cdk::println!("fn:build_index: cycles_used:{}", cycles_used);
+
+    // Create a new log entry
+    let log_entry = LogEntry {
+        timestamp: ic_cdk::api::time(), // Use the current timestamp
+        caller: ic_cdk::caller(),
+        function_name: "build_index".to_string(),
+        total_cycles: cycles_used,
+    };
+
+    // Append the log entry to the LOGS
+    LOGS.with(|logs| {
+        logs.borrow_mut().push(log_entry);
+    });
+    Ok(())
 }
 
 #[update]
@@ -73,6 +205,9 @@ fn delete_collection(name: String) -> Result<(), Error> {
 #[query]
 #[check_authorization]
 fn query(name: String, q: Vec<f32>, limit: i32) -> Result<Vec<String>, Error> {
+    let initial_cycles = canister_balance();
+    ic_cdk::println!("fn:query: initial_cycles:{}", initial_cycles);
+
     DB.with(|db| {
         let mut db = db.borrow_mut();
         let result = db.query(&name, q, limit);
@@ -80,6 +215,27 @@ fn query(name: String, q: Vec<f32>, limit: i32) -> Result<Vec<String>, Error> {
             Ok(data) => {
                 // Extract the Vec<(f32, String)> from the Ok variant
                 let (_, strings): (Vec<_>, Vec<_>) = data.into_iter().unzip();
+                // Get the final cycle count
+
+                let final_cycles = canister_balance();
+                ic_cdk::println!("fn:query: final_cycles:{}", final_cycles);
+                // Calculate the cycles used
+                let cycles_used = initial_cycles - final_cycles;
+                ic_cdk::println!("fn:query: cycles_used:{}", cycles_used);
+
+                // Create a new log entry
+                let log_entry = LogEntry {
+                    timestamp: ic_cdk::api::time(), // Use the current timestamp
+                    caller: ic_cdk::caller(),
+                    function_name: "query".to_string(),
+                    total_cycles: cycles_used,
+                };
+
+                // Append the log entry to the LOGS
+                LOGS.with(|logs| {
+                    logs.borrow_mut().push(log_entry);
+                });
+
                 // println!("Floats: {:?}", floats);
                 Ok(strings)
             }
